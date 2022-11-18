a2d0c85f11e7246c59ac189705f221f2c8b62ad1
Lida <108767144+LidiiaMashtepa@users.noreply.github.com>
2022-11-18 20:47:42 +0100

    task2

--- lesson-4/task-2-statistic/index.js
+++ lesson-4/task-2-statistic/index.js
@@ -5,12 +5,36 @@
 // - кількість елементів кожного тегу
 // Для роботи з класами рекомендується використовувати classList
 
+const container = document.querySelector(".container-mid");
+
 function collectDOMStat(root) {
   const stat = {
     tags: {},
     classes: {},
     texts: 0,
   };
+  const childNodes = root.childNodes;
+  console.log(childNodes);
+  for (const node of childNodes) {
+    console.log(node.nodeType);
+    if (node.nodeType === 3) {
+      stat.texts += 1;
+    } else if (node.nodeType === 1) {
+      const tag = node.tagName;
+      const classList = node.classList;
+      if (stat.tags[tag]) {
+        stat.tags[tag]++;
+      } else {
+        stat.tags[tag] = 1;
+      }
+      for (const c of classList) {
+        if (stat.classes[c]) {
+          stat.classes[c]++;
+        } else stat.classes[c] = 1;
+      }
+    }
+  }
 
   return stat;
 }
+console.log(collectDOMStat(container));
