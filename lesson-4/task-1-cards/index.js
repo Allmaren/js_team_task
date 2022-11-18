4276cfd7743ccd746f6da4f79d064bfc014b8c80
Alex-XAG <109016896+Alex-XAG@users.noreply.github.com>
2022-11-18 20:55:04 +0200

    task1

--- lesson-4/task-1-cards/index.js
+++ lesson-4/task-1-cards/index.js
@@ -1,1 +1,16 @@
-//Необхідно додати потрібну логіку, щоб при кліку на одну з карток, вона ставала активною (у нас для цього є клас active), а всі інші залишались/ставали неактивними
+//Необхідно додати потрібну логіку, щоб при кліку на одну з карток,
+// вона ставала активною(у нас для цього є клас active), а всі інші залишались / ставали неактивними
+const allImages = document.querySelectorAll(".slide");
+const mainContainer = document.querySelector(".container");
+
+mainContainer.addEventListener("click", activeClassHandler);
+
+function activeClassHandler(event) {
+  if (event.target.classList[0] === "slide") {
+    allImages.forEach((image) => {
+      image.classList.remove("active");
+    });
+
+    event.target.classList.add("active");
+  }
+}
