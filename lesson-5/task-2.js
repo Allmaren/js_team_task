822c9be795742ae41feb61e62d31bae2edd268b3
Alex-XAG <109016896+Alex-XAG@users.noreply.github.com>
2022-12-02 22:14:48 +0200

    task-2

--- lesson-5/task-2.js
+++ lesson-5/task-2.js
@@ -1,1 +1,15 @@
 //Напишіть функцію, яка виводитиме числа від 1 до 10 із затримкою в 1 секунду між кожним значенням
+
+function counter() {
+  const startCounter = 1;
+  const endCounter = 10;
+  let i = 0;
+  const timerId = setInterval(() => {
+    i += startCounter;
+    console.log(i);
+    if (i === endCounter) {
+      clearInterval(timerId);
+    }
+  }, 1000);
+}
+counter();
