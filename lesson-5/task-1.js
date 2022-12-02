f7055303a6ee67e96796eca19318d5de4858ddd7
Yevheniia Bohdan <AlmDomini@gmail.com>
2022-12-02 20:57:03 +0100

    v1

--- lesson-5/task-1.js
+++ lesson-5/task-1.js
@@ -1,1 +1,13 @@
 // Створіть функцію, яка отримує функцію як аргумент і виконує її через 2 секунди
+
+function timer(callback) {
+  setTimeout(() => {
+    callback();
+  }, 2000);
+}
+
+function c() {
+  console.log("Це я");
+}
+
+timer(c);
