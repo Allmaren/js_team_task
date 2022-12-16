ebe1f801245e82711bb3cf6c7dc8171bcef6c162
Yulia <nayanzina@gmail.com>
2022-12-16 21:58:44 +0200

    Update index.js

--- lesson6/index.js
+++ lesson6/index.js
@@ -1,15 +1,14 @@
 const refs = {
   form: document.querySelector(".form"),
   eventList: document.querySelector(".list"),
-  btnMore: document.querySelector(".more"),
+  btnMore: document.querySelector(".more")
 };
 
 const BASE_URL = "https://app.ticketmaster.com/discovery/v2/";
 const API_KEY = "9cTjAjlRB53wyhAFk5VzXcBu5GiPU6fK";
 
 let pageToFetch = 0;
-let queryToPage = '';
-
+let queryToPage = "";
 
 /*https://app.ticketmaster.com/discovery/v2/events.json?keyword=devjam&source=universe&countryCode=US&apikey={apikey} */
 
@@ -18,29 +17,28 @@ function fetchEvents(page, keyword) {
     apikey: API_KEY,
     page,
     keyword,
-    size: 10,
+    size: 10
   });
   return fetch(`${BASE_URL}events.json?${params}`)
-    .then((response) => {
+    .then(response => {
       if (response.status !== 200) {
         throw new Error(response.status);
       }
       return response.json();
     })
-    .catch((error) => {
+    .catch(error => {
       console.log(error);
     });
 }
 
 function getEvents(page, keyWord) {
-  fetchEvents(page, keyWord).then((result) => {
+  fetchEvents(page, keyWord).then(result => {
     console.log(result);
     const events = result._embedded.events;
     renderEvents(events);
-    refs.btnMore.classList.remove('btn_hidden');
+    refs.btnMore.classList.remove("btn_hidden");
   });
 }
-
 
 function renderEvents(events) {
   const markup = events
@@ -55,10 +53,13 @@ function renderEvents(events) {
   refs.eventList.insertAdjacentHTML("beforeend", markup);
 }
 
-refs.form.addEventListener('submit', (event) => {
- event.preventDefault();
- queryToPage = event.target.elements.search.value;
- 
- getEvents(pageToFetch,queryToPage );
+refs.form.addEventListener("submit", event => {
+  event.preventDefault();
+  queryToPage = event.target.elements.search.value;
 
-}) 
+  getEvents(pageToFetch, queryToPage);
+});
+
+refs.btnMore.addEventListener("click", () => {
+  getEvents(pageToFetch, queryToPage);
+});
